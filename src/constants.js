99b40378db92675345c4efdfa2c6720496af2ee5
GiorgioBrux <giorgiobrux2002@gmail.com>
2022-04-04 09:40:25 +0200

    feat: better config error

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,13 @@
 import merge from 'deepmerge-json';
 import defaultSettings from './constants-default.js';
-import customSettings from './constants-custom.js';
+
+let customSettings;
+try {
+    // eslint-disable-next-line import/no-unresolved
+    customSettings = await import('./constants-custom.js');
+} catch (e) {
+    throw new Error('Custom config missing or not a valid .js file.');
+}
 
 // eslint-disable-next-line import/no-mutable-exports
 let settings = defaultSettings;
