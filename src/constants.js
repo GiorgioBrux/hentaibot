45f0d77af1c876a275bbc73603a19289761c5ee5
Giorgio <giorgiobrux2002@gmail.com>
2022-04-14 22:34:02 +0200

    fix: broken config merge

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,6 @@ import defaultSettings from './constants-default.js';
 
 let customSettings;
 try {
-    // eslint-disable-next-line import/no-unresolved
     customSettings = await import('./constants-custom.js');
 } catch (e) {
     throw new Error('Custom config missing or not a valid .js file.');
@@ -11,6 +10,6 @@ try {
 
 // eslint-disable-next-line import/no-mutable-exports
 let settings = defaultSettings;
-settings = merge(settings, customSettings); // Plays better with autocompletion
+settings = merge(settings, customSettings.default); // Plays better with autocompletion
 
 export default settings;
